0100964c78d77dc05eb0fa27c2d1c9910261b1bd
ViraKryvets <vkrivets@gmail.com>
2023-06-24 00:07:11 +0300

    fix bug

--- app/services/order_creation_service.rb
+++ app/services/order_creation_service.rb
@@ -8,23 +8,15 @@ class OrderCreationService
 
   def create_order
     order = Order.new(@order_params)
-    subtotal = 0
 
     @cart.each do |product_id, quantity|
       product = Product.find(product_id)
-      # price = product.price
-      # subtotal += price * quantity
       order.product_orders.new(product: product, amount: quantity)
     end
 
-    # @total_amount = subtotal
-
     if order.save
-      @session[:cart] = nil # Clear the cart
+      @session[:cart] = nil
     end
-
     order
   end
-
-
 end
